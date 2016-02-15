6ef5369ef29579bb815882d5614868d057fc2b32
Maxime Béguin <maxime.beguin@vodalys-labs.com>
2016-02-15 14:48:42 +0100

    Correct bug when trying to validate / unvalidate a video
    Due to the new version of OpenVeo, video id have changed and it wasn't possible to validate / unvalidate a video anymore.

--- version.php
+++ version.php
@@ -12,7 +12,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_openveo_videos';
-$plugin->version = 2015112400;
+$plugin->version = 2016021500;
 $plugin->requires = 2013051409;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.0';
+$plugin->release = '1.0.1';
