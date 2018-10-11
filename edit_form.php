a5b6629daadd29de8e213ab4522f93766d957277
Maxime Béguin <maxime.beguin@veo-labs.com>
2018-10-11 16:34:20 +0200

    Remove extra documentation lines in edit form

--- edit_form.php
+++ edit_form.php
@@ -22,28 +22,28 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class block_openveo_videos_edit_form extends block_edit_form {
-    
+
     /**
      * Creates form fields specific to this type of block.
-     * 
+     *
      * @param MoodleQuickForm $mform The pear quick form form being built.
-     */    
+     */
     protected function specific_definition($mform) {
-        
+
         // Create a fieldset with a legend
         $mform->addElement('header', 'config_header', get_string('blocksettings', 'block'));
-        
+
         // Add an input text element to form
         $mform->addElement('text', 'config_title', get_string('inconfblocktitlelabel', 'block_openveo_videos'));
-        
+
         // Add help button
         $mform->addHelpButton('config_title', 'inconfblocktitlelabel', 'block_openveo_videos');
-        
+
         // Set input text default value
         $mform->setDefault('config_title', get_string('inconfblocktitlelabel', 'block_openveo_videos'));
-        
+
         // Clean title to plain text while submitting
         $mform->setType('config_title', PARAM_TEXT);
     }
-    
+
 }
